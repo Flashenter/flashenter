2b95e9ae58e9cead63f34c7a615f492c029a0bed
James Rivera <executiveworld101@gmail.com>
2026-06-15 18:14:08 -0400

    Use verified domain for email sender

--- api/send-email.js
+++ api/send-email.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      from: 'Flashenter <onboarding@resend.dev>',
+       from: 'Flashenter <notifications@flashenter.com>',
       to,
       subject,
       html
